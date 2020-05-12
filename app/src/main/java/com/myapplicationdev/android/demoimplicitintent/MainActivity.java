2dfac040c0ed935d60cf9cd56d3ee43e339db742
16046491 <16046491@myrp.edu.sg>
2020-05-12 21:57:01 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoimplicitintent/MainActivity.java
@@ -0,0 +1,54 @@
+package com.myapplicationdev.android.demoimplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etMsg;
+    Button btnEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etMsg = findViewById(R.id.editTextMessage);
+        btnEmail = findViewById(R.id.buttonEmail);
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //The action you want this intent to do;
+                //ACTION_SEND is used to indicate sending
+                // text
+                Intent email = new Intent(Intent.ACTION_SEND);
+
+                //Put essentials like email address, subject &
+                // body text
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[] {
+                                "jason_lim@rp.edu.sg"
+                        });
+                email.putExtra(Intent.EXTRA_SUBJECT,
+                        "Test Email from C347");
+                email.putExtra(Intent.EXTRA_TEXT,
+                        etMsg.getText());
+
+                //This MIME type indicates email
+                email.setType("message/rfc822");
+
+                //createChooser shows user a list of app that
+                // can handle this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client: "));
+            }
+        });
+    }
+}
